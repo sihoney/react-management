89bc392fcb03d28d043c106dea787f99181c450f
sihoney <hwangheewon@naver.com>
2021-09-03 14:27:43 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,55 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Customer from './components/Customer'
 
-function App() {
-  return (
-    <div className="gray-background">
-      <img src={logo} lat='logo' />
-      <h2>Let's develop management system!</h2>
-    </div>
-  );
+const users = [
+  {
+    id: 1,
+    image : 'https://placeimg.com/64/64/1',
+    name: 'hwang heewon',
+    birthday: 970414,
+    gender: 'female',
+    job: 'student'
+  },
+  {
+    id: 2,
+    image : 'https://placeimg.com/64/64/2',
+    name: 'hwang youjin',
+    birthday: 980317,
+    gender: 'female',
+    job: 'student'
+  },
+  {
+    id: 3,
+    image : 'https://placeimg.com/64/64/3',
+    name: 'lee jihyun',
+    birthday: 671120,
+    gender: 'female',
+    job: 'salary women'
+  }
+]
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        {
+          users.map(user => {
+            return (
+              <Customer 
+                key={user.id}
+                id={user.id}
+                image={user.image}
+                name={user.name}
+                birthday={user.birthday}
+                gender={user.gender}
+                job={user.job}/>
+            )
+          })
+        }
+      </div>
+      )
+  }
 }
 
 export default App;
