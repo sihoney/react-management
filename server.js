4c25197bbf52928f47dedd893948d39124d79b6a
sihoney <hwangheewon@naver.com>
2021-09-05 23:57:00 +0900

    Implement Customer Add Module & Customer List Refresh Module

--- server.js
+++ server.js
@@ -24,13 +24,34 @@ const connection = mysql.createConnection({
 })
 connection.connect();
 
+const multer = require('multer');
+const upload = multer({ dest: './upload' })
+
 app.get('/api/customers', (req, res) => {
     connection.query(
         "SELECT * FROM CUSTOMER", 
         (err, rows, fields) => {
             res.send(rows);
+            console.log('get works')
         }
     )
+})
+
+app.use('/image', express.static('./upload'));
+// 업로드 폴더를 사용자가 접근해서 프로필 이미지를 확인할 수 있도록 하기 위해서 express.static 이용 
+
+app.post('/api/customers', upload.single('image'), (req, res) => {
+    let sql = 'INSERT INTO CUSTOMER VALUES (null, ?, ?, ?, ?, ?)';
+    let image = '/image/' + req.file.filename;
+    let name = req.body.name;
+    let birthday = req.body.birthday;
+    let gender = req.body.gender;
+    let job = req.body.job;
+    let params = [image, name, birthday, gender, job];
+    connection.query(sql, params, (err, rows, fields)=> {
+        res.send(rows);
+        console.log(err)
+    })
 })
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
